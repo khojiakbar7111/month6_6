2271cdda4273f9db094fcebc3f64b8efb018f964
hojiakbar7111 <hojiakbarnematov33@gmail.com>
2023-11-24 15:33:23 +0500

    i can use router

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import { getStore } from './utils'
 import { uid } from 'uid'
 import Add from './components/Add'
 import Products from './components/Products'
+import { Route, Router, Routes } from 'react-router-dom'
+import Home from './pages/home'
+import Hero from './pages/Hero'
 
 
 
@@ -76,9 +79,21 @@ function App() {
     <>
     <h2>{users.uname && 'Welcome'} {users.uname}</h2>
 <Navabr login={ login} setLogin={setLogin}/>
-{ login && <Login name={name} setName={setName} email={email} setEmail={setEmail}  handleSubmit={handleSubmit}/>}
-<Add  edit={edit} setEdit={setEdit}  pname={pname} setPname={setPname} price={price} setPrice={setPrice} addProduct={ addProduct}/>
-   <Products ediItem={ediItem} setEdit={setEdit} products={ products} deleteItem={deleteItem}   />
+
+   <Routes>
+    <Route path='login' element= { login && <Login name={name} setName={setName} email={email} setEmail={setEmail}  handleSubmit={handleSubmit}/>}/>
+
+    <Route path='/' element={<Home/>}/>
+    <Route path='/hero'  element={<Hero/>}/>
+    <Route path='/products' element={<Products ediItem={ediItem} setEdit={setEdit} products={ products} deleteItem={deleteItem}   />}/>
+    <Route path='/add' element={<Add  edit={edit} setEdit={setEdit}  pname={pname} setPname={setPname} price={price} setPrice={setPrice} addProduct={ addProduct}/>}/>
+    <Route path='/erorr' element={<Error/>}/>
+   </Routes>
+
+
+{/* { login && <Login name={name} setName={setName} email={email} setEmail={setEmail}  handleSubmit={handleSubmit}/>} */}
+{/* <Add  edit={edit} setEdit={setEdit}  pname={pname} setPname={setPname} price={price} setPrice={setPrice} addProduct={ addProduct}/> */}
+   {/* <Products ediItem={ediItem} setEdit={setEdit} products={ products} deleteItem={deleteItem}   /> */}
     </>
   )
 }
--- src/components/Navabr.jsx
+++ src/components/Navabr.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navabr({login, setLogin}) {
   return (
     <div>
         <ul className='nav'>
-   <li>hero</li>
-   <li>hero</li>
-   <li>Products</li>
    <li>
-    <button className='nav_btn' onClick={() => setLogin(!login)}>login</button>
+    <Link to='/'>
+    home</Link>
+   </li>
+   <li>
+    <Link to='/hero'>Hero</Link>
+   </li>
+   <li>
+    <Link to='/products'>
+    Products</Link>
+   </li>
+   <li>
+    <Link to='/login'>
+<button className='nav_btn' onClick={() => setLogin(!login)}></button>
+    </Link>
+    login</button>
    </li>
         </ul>
     </div>
